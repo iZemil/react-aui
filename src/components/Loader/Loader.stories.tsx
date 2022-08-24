5a86d53b9207c82dd2895636716de0ff1c221fd5
emil zari <3zemil@gmail.com>
2022-08-24 13:12:44 +0300

    fix: Loader with skeleton

--- src/components/Loader/Loader.stories.tsx
+++ src/components/Loader/Loader.stories.tsx
@@ -1,6 +1,51 @@
 import { Loader } from '.';
+import * as React from 'react';
+
+import { Button } from '../Button';
+import Container from '../Container';
 
 export default {
-	title: 'Loader',
 	component: Loader,
+};
+
+export const Basic = () => {
+	return (
+		<Container.$ column>
+			<Loader />
+
+			<Loader size={10} relative />
+
+			<Loader size={2} relative />
+		</Container.$>
+	);
+};
+
+export const Content = () => {
+	const [isLoaded, setLoaded] = React.useState(false);
+
+	return (
+		<>
+			<Button onClick={() => setLoaded(!isLoaded)}>Toggle loading</Button>
+
+			<Container.$ column>
+				<Loader active={!isLoaded}>
+					<p>First loader content</p>
+				</Loader>
+
+				<Loader
+					active={!isLoaded}
+					skeleton={
+						<p>
+							Any skeleton lib:
+							<a href="https://www.npmjs.com/package/react-loading-skeleton">
+								Example: react-loading-skeleton
+							</a>
+						</p>
+					}
+				>
+					<p>Skeleton loader content</p>
+				</Loader>
+			</Container.$>
+		</>
+	);
 };
--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -1,49 +1,74 @@
+import * as React from 'react';
 import styled from 'styled-components';
 
 import { color } from '../../styles';
 
-const HEIGHT = 4;
-const StLoader = styled.div`
-	@keyframes loader {
-		0% {
+const DEFAULT_SIZE = 4;
+
+export interface ILoaderProps {
+	active?: boolean;
+	size?: number;
+	relative?: boolean;
+	children?: React.ReactNode;
+	skeleton?: React.ReactNode;
+}
+
+const S = {
+	$: styled.div<ILoaderProps>`
+		@keyframes loader {
+			0% {
+				width: 0;
+			}
+
+			50% {
+				width: 50%;
+			}
+
+			100% {
+				width: 100%;
+			}
+		}
+
+		position: ${(props) => (props.relative ? 'relative' : 'absolute')};
+		left: 0;
+		top: 0;
+		height: ${(props) => props.size ?? DEFAULT_SIZE}px;
+		overflow: hidden;
+		width: 100%;
+	`,
+
+	Bar: {
+		$: styled.div`
+			background: ${color('bg')};
+			height: 100%;
+		`,
+	},
+
+	Progress: {
+		$: styled.div`
+			animation: loader 2s ease infinite;
+			background: ${color('main')};
 			width: 0;
-		}
+			height: 100%;
+		`,
+	},
+};
 
-		50% {
-			width: 50%;
-		}
+export const Loader = (props: ILoaderProps) => {
+	const { active = true, skeleton, children } = props;
 
-		100% {
-			width: 100%;
-		}
-	}
-
-	position: absolute;
-	left: 0;
-	top: 0;
-	height: ${HEIGHT}px;
-	overflow: hidden;
-	width: 100%;
-`;
-
-const StLoaderBar = styled.div`
-	background: ${color('bg')};
-	height: 100%;
-`;
-
-const StLoaderProgress = styled.div`
-	animation: loader 2s ease infinite;
-	background: ${color('main')};
-	width: 0;
-	height: 100%;
-`;
-
-export const Loader = () => {
 	return (
-		<StLoader>
-			<StLoaderBar>
-				<StLoaderProgress />
-			</StLoaderBar>
-		</StLoader>
+		<>
+			{active &&
+				(skeleton ?? (
+					<S.$ {...props}>
+						<S.Bar.$>
+							<S.Progress.$ />
+						</S.Bar.$>
+					</S.$>
+				))}
+
+			{!active && children}
+		</>
 	);
 };
