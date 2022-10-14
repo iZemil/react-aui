4a3c0923eba959fb3e872b54994e6b40d1485e7a
zemil <3zemil@gmail.com>
2022-10-14 23:32:22 +0300

    fix: icons

--- src/components/Icon/utils.tsx
+++ src/components/Icon/utils.tsx
@@ -1,10 +1,14 @@
 import * as React from 'react';
-import { IconType } from 'react-icons';
+import type { IconType } from 'react-icons';
+// TODO: split react-icons
+// import('react-icons/bs').then((bs) => {
+// 	Object.assign(BS_SET, bs);
+// });
+import * as bs from 'react-icons/bs';
 
 import { TSize, getFontSize } from '../../styles';
 
-import { EmptyIcon } from './EmptyIcon';
-import { BSIcons, BSSet, BaseIconNames, BaseSet } from './types';
+import { BSSet, BaseSet } from './types';
 
 interface SvgIconProps {
 	size?: TSize;
@@ -38,19 +42,7 @@ export const BASE_SIZES = {
 };
 
 // link to react-icons/bs
-export const BS_SET = new Proxy({} as BSSet, {
-	get(bsIcons: BSSet, name: BSIcons) {
-		if (name in bsIcons) {
-			return bsIcons[name];
-		}
-
-		return <EmptyIcon />;
-	},
-});
-
-import('react-icons/bs').then((bs) => {
-	Object.assign(BS_SET, bs);
-});
+export const BS_SET = bs;
 
 export function createIconAliases<TAlias extends string>(
 	fn: (bsIcons: BSSet) => Record<TAlias, IconType>
@@ -58,60 +50,49 @@ export function createIconAliases<TAlias extends string>(
 	return fn(BS_SET);
 }
 
-export const BASE_SET: BaseSet = new Proxy(
-	createIconAliases((bs) => ({
-		Italic: bs.BsTypeItalic,
-		AddHeading: bs.BsTypeH3,
-		Bold: bs.BsTypeBold,
-		Markdown: bs.BsMarkdown,
-		Sun: bs.BsSun,
-		Moon: bs.BsMoonStars,
-		Home: bs.BsHouse,
-		Link: bs.BsLink45Deg,
-		Pin: bs.BsPinAngle,
-		Delete: bs.BsTrash,
-		ReadMode: bs.BsEyeglasses,
-		AddCheckbox: bs.BsListCheck,
-		AddListItem: bs.BsListUl,
-		AddImage: bs.BsImage,
-		AddCode: bs.BsCodeSlash,
-		Checked: bs.BsFillCheckSquareFill,
-		NotChecked: bs.BsSquare,
-		Info: bs.BsQuestionCircle,
-		Pro: bs.BsTrophy,
-		Calendar: bs.BsCalendarRange,
-		ChevronLeft: bs.BsChevronLeft,
-		ChevronRight: bs.BsChevronRight,
-		Clip: bs.BsPaperclip,
-		ThreeDots: bs.BsThreeDotsVertical,
-		Person: bs.BsPerson,
-		Percent: bs.BsPercent,
-		Chat: bs.BsChat,
-		EmptyAvatar: bs.BsPersonCircle,
-		Ok: bs.BsCheck2,
-		Copy: bs.BsFiles,
-		New: bs.BsPlusCircle,
-		Edit: bs.BsPencil,
-		Play: bs.BsPlayCircle,
-		Pause: bs.BsPauseCircle,
-		Settings: bs.BsGear,
-		Hide: bs.BsEyeSlash,
-		Accept: bs.BsCheck,
-		Clock: bs.BsClock,
-		Stop: bs.BsStopCircle,
-		ArrowRight: bs.BsArrowRight,
-		ArrowLeft: bs.BsArrowLeft,
-		SignOut: bs.BsDoorOpen,
-		ChevronDown: bs.BsChevronDown,
-		ChevronUp: bs.BsChevronUp,
-	})),
-	{
-		get(icons: BaseSet, name: BaseIconNames) {
-			if (name in icons) {
-				return icons[name];
-			}
-
-			return <EmptyIcon />;
-		},
-	}
-);
+export const BASE_SET: BaseSet = createIconAliases((bs) => ({
+	Italic: bs.BsTypeItalic,
+	AddHeading: bs.BsTypeH3,
+	Bold: bs.BsTypeBold,
+	Markdown: bs.BsMarkdown,
+	Sun: bs.BsSun,
+	Moon: bs.BsMoonStars,
+	Home: bs.BsHouse,
+	Link: bs.BsLink45Deg,
+	Pin: bs.BsPinAngle,
+	Delete: bs.BsTrash,
+	ReadMode: bs.BsEyeglasses,
+	AddCheckbox: bs.BsListCheck,
+	AddListItem: bs.BsListUl,
+	AddImage: bs.BsImage,
+	AddCode: bs.BsCodeSlash,
+	Checked: bs.BsFillCheckSquareFill,
+	NotChecked: bs.BsSquare,
+	Info: bs.BsQuestionCircle,
+	Pro: bs.BsTrophy,
+	Calendar: bs.BsCalendarRange,
+	ChevronLeft: bs.BsChevronLeft,
+	ChevronRight: bs.BsChevronRight,
+	Clip: bs.BsPaperclip,
+	ThreeDots: bs.BsThreeDotsVertical,
+	Person: bs.BsPerson,
+	Percent: bs.BsPercent,
+	Chat: bs.BsChat,
+	EmptyAvatar: bs.BsPersonCircle,
+	Ok: bs.BsCheck2,
+	Copy: bs.BsFiles,
+	New: bs.BsPlusCircle,
+	Edit: bs.BsPencil,
+	Play: bs.BsPlayCircle,
+	Pause: bs.BsPauseCircle,
+	Settings: bs.BsGear,
+	Hide: bs.BsEyeSlash,
+	Accept: bs.BsCheck,
+	Clock: bs.BsClock,
+	Stop: bs.BsStopCircle,
+	ArrowRight: bs.BsArrowRight,
+	ArrowLeft: bs.BsArrowLeft,
+	SignOut: bs.BsDoorOpen,
+	ChevronDown: bs.BsChevronDown,
+	ChevronUp: bs.BsChevronUp,
+}));
