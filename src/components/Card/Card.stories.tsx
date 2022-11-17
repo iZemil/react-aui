e91f8bbf087adcac09ae473ff31b520ffb431427
zemil <3zemil@gmail.com>
2022-11-17 12:24:10 +0300

    fix: card updates

--- src/components/Card/Card.stories.tsx
+++ src/components/Card/Card.stories.tsx
@@ -1,6 +1,7 @@
 import { Card } from '.';
 import * as React from 'react';
 
+import { wait } from '../../utils';
 import Container from '../Container';
 
 export default {
@@ -8,6 +9,17 @@ export default {
 };
 
 export const Basic = () => {
+	const [hidden, setHidden] = React.useState(false);
+	const handleHidden = async () => {
+		setHidden(true);
+
+		if (hidden == false) {
+			await wait(1000);
+
+			setHidden(false);
+		}
+	};
+
 	return (
 		<Container column>
 			<Card>
@@ -17,6 +29,17 @@ export const Basic = () => {
 			</Card>
 
 			<Card color="main">
+				<div>Row</div>
+				<div>Row</div>
+				<div>Row</div>
+			</Card>
+
+			<Card hidden={hidden} onClick={handleHidden} row>
+				<div>Row</div>
+				<div>Click me</div>
+			</Card>
+
+			<Card dashed color="blue">
 				<div>Row</div>
 				<div>Row</div>
 				<div>Row</div>
--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -5,23 +5,58 @@ import { IAUI, TColors, TSize, border, borderRadius, getAnyColor, padding } from
 export interface CardProps {
 	size?: TSize;
 	color?: TColors | string;
+	dashed?: boolean;
+	hidden?: boolean;
+	row?: boolean;
 }
 
 export const Card = styled.div<CardProps>`
 	display: flex;
-	flex-direction: column;
+	${(props) => (props.row ? `align-items: center;` : `flex-direction: column;`)}
 	gap: ${(props) => padding(props.size ?? 'medium')};
 	padding: ${(props) => padding(props.size ?? 'medium')};
-	border: ${border('grey')};
 	border-radius: ${borderRadius};
-	background: ${(props) => {
-		const theme = props.theme as IAUI;
 
-		return getAnyColor(theme, props.color) ?? 'transparent';
-	}};
+	${(props) => {
+		if (props.dashed) {
+			return `
+				border-style: dashed;
+				border-width: 2px;
+				box-shadow: none;
+				background: transparent !important;
+			`;
+		}
+
+		return `
+			border: ${border('grey')(props)};
+		`;
+	}}
+
+	${(props) => {
+		if (props.color) {
+			const theme = props.theme as IAUI;
+			const colorVal = getAnyColor(theme, props.color);
+
+			return `
+				background: ${colorVal};
+				border-color: ${colorVal};
+			`;
+		}
+
+		return ``;
+	}}
+
+	${(props) => {
+		if (props.hidden) {
+			return `
+				display: none;
+			`;
+		}
+
+		return ``;
+	}}
 
 	@media screen and (max-width: 480px) {
-		padding: ${padding('small')};
-		border: 0;
+		padding: ${padding('medium')};
 	}
 `;
