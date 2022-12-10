7a88bd438cbcdd50eff72f4adcf84ab094cca67e
zemil <3zemil@gmail.com>
2022-12-11 02:41:03 +0300

    fix: bundler

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -511,14 +511,6 @@ module.exports = function (webpackEnv) {
 			// This gives some necessary context to module not found errors, such as
 			// the requesting resource.
 			new ModuleNotFoundPlugin(paths.appPath),
-			// Makes some environment variables available to the JS code, for example:
-			// if (process.env.NODE_ENV === 'production') { ... }. See `./env.js`.
-			// It is absolutely essential that NODE_ENV is set to production
-			// during a production build.
-			// Otherwise React will be compiled in the very slow development mode.
-			new webpack.DefinePlugin({
-				'process.env.NODE_ENV': 'production',
-			}),
 			// Experimental hot reloading for React .
 			// https://github.com/facebook/react/tree/main/packages/react-refresh
 			isEnvDevelopment &&
